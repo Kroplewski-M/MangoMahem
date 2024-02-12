4d90d60e9042f0998fd7b5950c8424c68a8051a1
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-02-12 18:39:26 +0000

    started register page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -36,7 +36,7 @@ export const Nav = () => {
     <nav className={`w-[100vw] h-[70px] bg-secondary duration-200 ease-linear flex relative z-[100] sticky top-0 shadow-lg`}>
       <div className="self-center flex pl-[10px] hover:cursor-pointer">
         <LogoSVG width={50} height={50} />
-        <p className="font-bold self-center pl-2 text-PrimaryText">Mango Mahem</p>
+        <p className="font-bold self-center pl-2 text-PrimaryText">Mango Mayhem</p>
       </div>
       <div className={`flex-row ${openNav ? "fixed top-0 right-0 bg-secondary w-[90%] h-[100vh] text-center pt-10 animate-SlideIn" : "hidden"} md:inline-flex self-center w-[380px] mx-auto`}>
         <div className="absolute right-5 top-5 hover:cursor-pointer md:hidden" onClick={() => setOpenNav(false)}>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,50 @@
+import { FruitBowlSVG } from "../assets/SVG/FruitBowlSVG";
+
 export const Register = () => {
   return (
-    <section className="w-[100vw] min-h-[100vh] flex">
-      <div className="w-[50%] h-[100vh] bg-blue-200"></div>
-      <div className="w-[50%] h-[100vh] bg-red-200"></div>
+    <section className="w-[100vw] min-h-[100vh] flex flex-col flex-col-reverse md:flex-row">
+      <div className="md:w-[50%] w-[100%] h-[100vh] bg-secondary/50 flex relative">
+        <div className="flex self-center flex-col text-center w-[100%] -mt-16">
+          <div className="width-[300px] height-[300px] mx-auto absolute opacity-[0.5]">
+            <FruitBowlSVG width={300} height={300} fill="#FFFFFF" />
+          </div>
+          <p className="text-[40px] font-bold text-PrimaryText z-[50]">Already have an account?</p>
+          <p className="text-[30px] font-bold text-PrimaryText z-[50]">Log in now</p>
+          <div className="mx-auto w-[150px] h-[35px] mt-5 z-[50]">
+            <button className="w-[100%] h-[100%] bg-secondary hover:font-bold rounded shadow-md ">Log in</button>
+          </div>
+        </div>
+      </div>
+      <div className="md:w-[50%] w-[100%] h-[100vh] bg-red-200 flex relative">
+        <div className="flex self-center flex-col text-center w-[100%]">
+          <p className="text-[40px] font-bold text-PrimaryText z-[50]">Register an account</p>
+          <form action="">
+            <div>
+              <label htmlFor="firstname">First name</label>
+              <input type="text" name="firstname" />
+            </div>
+            <div>
+              <label htmlFor="lastname">Last name</label>
+              <input type="text" name="lasttname" />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+            </div>
+            <div>
+              <label htmlFor="passwordConf">Confirm password</label>
+              <input type="text" name="passwordConf" />
+            </div>
+            <div>
+              <button>Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </section>
   );
 };
