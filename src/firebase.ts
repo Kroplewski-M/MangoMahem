687aefee2fb8d9c57168da9f956150fa63adf30d
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-02-15 14:33:31 +0000

    added npm firebase

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIRBASE_API_KEY,
+  authDomain: "mangomayhem-c0cfc.firebaseapp.com",
+  projectId: "mangomayhem-c0cfc",
+  storageBucket: "mangomayhem-c0cfc.appspot.com",
+  messagingSenderId: "909542903614",
+  appId: "1:909542903614:web:156d1d67b5d7e2ba9e266f",
+  measurementId: "G-5NYJK68HJW",
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const analytics = getAnalytics(app);
+export const storage = getStorage();
+export const db = getFirestore();
