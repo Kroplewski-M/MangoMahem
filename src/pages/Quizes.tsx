c102ef76f8a144e2beac037dc5407c48c6bd52dc
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-02-27 09:19:24 +0000

    updated quizes page

--- src/pages/Quizes.tsx
+++ src/pages/Quizes.tsx
@@ -1,12 +1,19 @@
-import { useUserInfo } from "../context/UserContext";
+import { SearchSVG } from "../assets/SVG/SearchSVG";
 
 export const Quizes = () => {
-  const { userInfo } = useUserInfo();
   return (
-    <div>
-      <p>{userInfo.displayName}</p>
-      <p>{userInfo.email}</p>
-      <p>{userInfo.score}</p>
+    <div className="md:px-16 pt-5">
+      <div className="flex rounded bg-PrimaryText w-[340px] pl-[5px] ml-[3px] md:ml-0">
+        <div className="w-[30px] height-[30px] flex self-center">
+          <SearchSVG width={30} height={30} fill="#FFA466" />
+        </div>
+        <div className="flex">
+          <input type="text" className="w-[300px] h-[40px] ml-[5px] text-gray-200 font-bold rounded-md pl-[5px] bg-transparent focus:outline-none" placeholder="Search for a quiz" />
+          <div className="w-[20px] h-[20px] rounded-full bg-red-700 hover:cursor-pointer ml-[5px] mt-[7px]">
+            <p className="text-gray-100 font-bold text-center pt-[2px] text-[12px]">X</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
