50bac1302a77eb05090698e6216be68dfc1f6204
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-03-15 20:16:28 +0000

    UI changes to quiz info

--- src/pages/QuizInfo.tsx
+++ src/pages/QuizInfo.tsx
@@ -29,15 +29,15 @@ export const QuizInfo = () => {
     }
   }
   return (
-    <div className="md:w-[400px] w-[90%] mx-auto min-h-[400px] pb-5 rounded-md bg-secondary mt-16">
+    <div className="md:w-[500px] w-[90%] mx-auto min-h-[400px] pb-5 rounded-md bg-secondary mt-16">
       {!startQuiz ? (
         <>
           <p className="font-bold text-PrimaryText text-[25px] text-center">{quizInfo?.Name}</p>
-          <div className="w-[80%] flex place-content-around mx-auto mt-[10px]">
+          <div className="w-[80%] flex place-content-between mx-auto mt-[10px]">
             <p className="text-[12px]">Created:{quizInfo?.CreatedAt.toString().substring(0, 10)}</p>
             <p className="text-[12px]">By:{quizInfo?.CreatedBy}</p>
           </div>
-          <div className="w-[250px] h-[200px] mx-auto mt-[10px]">
+          <div className="w-[350px] h-[300px] mx-auto mt-[10px]">
             <img src={quizInfo?.Image} alt={quizInfo?.Name} className="w-[100%] h-[100%] rounded-md" />
           </div>
           <div className="w-[80%] mx-auto mt-[20px]">
@@ -52,7 +52,26 @@ export const QuizInfo = () => {
           </div>
         </>
       ) : (
-        <></>
+        <>
+          <div className="w-[100%] h-[70px] rounded-t-md bg-PrimaryText">
+            <p className="text-gray-200 text-center font-bold text-[25px] pt-[15px]">
+              Question {currentQuestion + 1} out of {quizInfo?.Questions.length}
+            </p>
+          </div>
+          <p className="text-center text-[20px] pt-5 font-bold">{quizInfo?.Questions[currentQuestion].QuestionName} ?</p>
+          <div className="mt-5">
+            {quizInfo?.Questions[currentQuestion].Answers.map((answer, index) => (
+              <div key={index} className="w-[90%] mx-auto h-[50px] bg-mainBg hover:cursor-pointer grid place-content-center rounded-md mt-[10px] hover:border-2 border-PrimaryText">
+                <p className="">{answer.AnswerText}</p>
+              </div>
+            ))}
+          </div>
+          <div className="w-[120px] h-[40px] mx-auto mt-5">
+            <button className="w-[100%] h-[100%] bg-PrimaryText rounded-md text-gray-200" onClick={() => setCurrentQuestion(currentQuestion + 1)}>
+              next question
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
