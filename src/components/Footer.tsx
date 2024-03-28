426b5500ff939e0f338e1ec5ee1feefc9f159012
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-03-28 16:19:10 +0000

    typo on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="w-[100vw] h-[70px] bg-secondary text-center pt-[10px]">
       <p className="font-bold text-[#333333]">Team Mango</p>
-      <p className="font-bold text-[#333333]">Team Project Assigment</p>
+      <p className="font-bold text-[#333333]">Team Project Assignment</p>
     </div>
   );
 };
